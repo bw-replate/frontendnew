ceb9ff6ef7f283ba03889beaa1d9d39c9a361077
fuston05 <fuston05@yahoo.com>
2020-03-03 23:00:10 -0500

    got error and succes messages handled on signup form, on success it renders a button to go to the login page

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,112 +2,110 @@ import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
 import axios from 'axios';
 import * as Yup from 'yup';
-import {useHistory} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //styles
-import {Error, Message} from './SignupStyles';
+import { Error, Message } from './SignupStyles';
 
-const Registration = ({values, errors, touched, status }) => {
-  const [error, setError]= useState('');
-  const [message, setMessage]= useState('');
-  const history= useHistory();
+const Registration = ({ values, errors, touched, status }) => {
+  const [isError, setIsError] = useState(false);
+  const [message, setMessage] = useState('');
 
-    useEffect(() =>{
-      if(typeof status === 'string'){
-        setError(status);
-      }else if(!status === ""){
-        setError('');
-        status && values.setUser({
-          ...values.user, ...status
-        });
-  
-      }//end if/else
-    }, [status])
-    return (
-        <div className="user-form">
+  useEffect(() => {
+    console.log('status: ', status);
+    if ( typeof status === 'object' && status !== undefined) {
+      status && values.setUser({
+        ...values.user, ...status
+      });
+      setIsError(false);
+      setMessage('Successful Registration');
+    }else if( status === 'duplicate' ){
+      setIsError(true);
+      setMessage('That user already exists');
+    }
 
-          {console.log('user: ', values.user)}
+  }, [status])
+  return (
+    <div className="user-form">
 
-            <Form>
-                <label htmlFor="username">
-                    Name
+      {console.log('user: ', values.user)}
+
+      <Form>
+        <label htmlFor="username">
+          Name
                     <Field
-                        id="username"
-                        type="text"
-                        name="username"
-                        placeholder="username"
-                    />
-                    {touched.username && errors.username && (<p classusername="errors">{errors.name}</p>)}
-                </label>
-                <label htmlFor="password">
-                    Password
+            id="username"
+            type="text"
+            name="username"
+            placeholder="username"
+          />
+          {touched.username && errors.username && (<p classusername="errors">{errors.name}</p>)}
+        </label>
+        <label htmlFor="password">
+          Password
                     <Field
-                        id="password"
-                        type="password"
-                        name="password"
-                        placeholder="password"
-                    />
-                    {touched.password && errors.password && (<p className="errors">{errors.password}</p>)}
-                </label>
-                <label htmlFor="phoneNumber">
-                    Phone
+            id="password"
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+          {touched.password && errors.password && (<p className="errors">{errors.password}</p>)}
+        </label>
+        <label htmlFor="phoneNumber">
+          Phone
                     <Field
-                        id="phoneNumber"
-                        type="text"
-                        name="phoneNumber"
-                        placeholder="Phone"
-                    />
-                    {touched.phoneNumber && errors.phoneNumber && (<p className="errors">{errors.phoneNumber}</p>)}
-                </label>
-               
-                <button type="submit">Register</button>
-            </Form>
-              {error ? <Error className= 'error'>{error}</Error> : null }
-              {message ? <Message className= 'message'>{message}</Message> : null }
-            { 
-              <>
-                <p><i>testing purposes only:</i></p>
-                <p>useName: {values.username}</p>
-                <p>password: {values.password}</p>
-                <p>phone: {values.phoneNumber}</p>
-              </>
-            }
-        </div>
-    );
+            id="phoneNumber"
+            type="text"
+            name="phoneNumber"
+            placeholder="Phone"
+          />
+          {touched.phoneNumber && errors.phoneNumber && (<p className="errors">{errors.phoneNumber}</p>)}
+        </label>
+
+        <button type="submit">Register</button>
+      </Form>
+      { // if there are errors show them
+        isError ? <Error className='error'>{message}</Error> :
+        //if theres a message but no errors its a succes message, show it
+          message.length > 1 && isError === false ? <Message className='message'>{message}</Message> : null
+      }
+      { //if successful registration show button to goto log in page 
+      message === 'Successful Registration' ? <Link to= '/'><button>Log In</button></Link> : null}
+    </div>
+  );
 };
 
 const FormikRegistration = withFormik({
-    mapPropsToValues(props) {
-        return {
-            username: props.user.username || '',
-            phoneNumber: props.user.phoneNumber || '',
-            password: props.user.password || '',
-            // tos: props.tos || false,
-            user: props.user,
-            setUser: props.setUser
-        };         
-    },
-    validationSchema: Yup.object().shape({
-        username: Yup.string().required(),
-        phoneNumber: Yup.string().required(),
-        password: Yup.string().required(),
-        // tos: Yup.bool().oneOf([true],"Please accept Terms of Service to Continue").required()
-    }),
-    handleSubmit(values, { setStatus, resetForm}) {
-        axios.post('https://bw-replate-1.herokuapp.com/api/auth/register', values)
-                .then(res => {
-                    setStatus(res);
-                    console.log('res: ', res);
-                    resetForm();
-                })
-                .catch(err => {
-                  if (err.response.status === 500){
-                    setStatus('That user already exists');
-                  }else{
-                    console.log('Error: ', err.response);
-                  }
-                  // console.log('Error: ', err.response.status)
-                })
-    }
+  mapPropsToValues(props) {
+    return {
+      username: props.user.username || '',
+      phoneNumber: props.user.phoneNumber || '',
+      password: props.user.password || '',
+      user: props.user,
+      setUser: props.setUser
+    };
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required(),
+    phoneNumber: Yup.string().required(),
+    password: Yup.string().required(),
+    // tos: Yup.bool().oneOf([true],"Please accept Terms of Service to Continue").required()
+  }),
+  handleSubmit(values,{setStatus, resetForm }) {
+    axios.post('https://bw-replate-1.herokuapp.com/api/auth/register', values)
+      .then(res => {
+        setStatus(res);
+        resetForm();
+      })
+      .then( () => {setStatus('')} )
+      .catch(err => {
+        if (err.response.status === 500) {
+          setStatus('duplicate');
+          resetForm();
+        }
+        // console.log('Error: ', err.response.status);
+      })
+      setStatus('');
+  }//end handleSubmit
 })(Registration)
 export default FormikRegistration;
