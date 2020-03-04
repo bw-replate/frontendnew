1df7d4112373929111ff309fb2088c0a107a9ac9
wildcard329 <enkiduwildman@gmail.com>
2020-03-04 00:01:46 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -10,52 +10,27 @@ import {UserContext} from './Contexts/UserContext';
 //imports
 import Header from './components/Header/Header';
 import FormikRegistration from './components/Signup/Signup';
-import Profile from './components/Profile/Profile';
-
 
 //styles
 import './App.css';
 
 function App() {
-  const [user, setUser]= useState({
-    username: '',
-    password: '',
-    phoneNumber: ''
-  });
+  const [user, setUser]= useState();
 
   return (
+
+
       <div className="App">
-        {/* <UserContext.Provider value= {user}> */}
+        <UserContext.Provider value= {user}>
           <Header/>
 
           {/* site heading */}
-        <Route exact path="/">
-        <h1 className="mainHeading"> Replate </h1>
-          <Login/>
-        </Route>
-
-        <Route exact path="/profile">
-        <h1 className="mainHeading"> Profile </h1>
-          <Profile/>
-        </Route>
+          <h1> Replate </h1>
 
           {/* login component */}
-<<<<<<< HEAD
-          <FormikRegistration 
-            setUser= {setUser} 
-            user= {user} 
-            />
-        </UserContext.Provider>
-        <Route exact path="/busines" component={Business} />
-=======
-
-        <Route path="/register">
-        <h2 className="mainHeading"> Register Below </h2>
           <FormikRegistration user= {user} setUser= {setUser} />
-        {/* </UserContext.Provider> */}
-        </Route>
-
->>>>>>> 44f70b701490658bf47aaa604f242cd02d3825fa
+        </UserContext.Provider>
+        <Route exact path="/business" component={Business} />
       </div>
   );
 }//end App
