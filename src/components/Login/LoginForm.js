2340fe6a85808a888aca0c48b0e2458ba709686f
fuston05 <fuston05@yahoo.com>
2020-03-04 07:25:09 -0500

    added login functionality, added success and error message displays, and redirects to profile page on successful login

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {useHistory} from 'react-router-dom';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
@@ -7,19 +8,39 @@ import {axiosWithAuth} from '../../utils/axiosWithAuth';
 
 //components
 
+//stlyles
+import {Error, Message} from './LoginStyles';
+
 const LoginForm = ({ values, touched, errors, status }) => {
   const [user, setUser] = useState({
     usename: '',
     password: ''
   });
+  const [isError, setIsError] = useState(false);
+  const [message, setMessage] = useState('');
+  const history= useHistory();
 
   useEffect(() => {
-    status && console.log("status has changes", status.token);
-    status && setUser({
-      ...user,
-      status
-    });
-    status && window.localStorage.setItem('token', status.token);
+    // status && console.log("status:", status);
+    //if status contains a response, and is not undefined... proceed
+    if( typeof status === 'object' && status !== undefined ){
+      setIsError(false);
+      status && setUser({
+        ...user,
+        status
+      });
+      status && window.localStorage.setItem('token', status.token);
+      setIsError(false);
+      setMessage('Successful Registration');
+      setTimeout( () => {
+        //redirect
+        history.push('/profile');  
+      },1000 );//end setTimeout
+    }else if( status === 'error' ){
+      setIsError(true);
+      setMessage('Username or password is incorrect');
+    }//end if
+   
 
   }, [status]);
 
@@ -49,16 +70,21 @@ const LoginForm = ({ values, touched, errors, status }) => {
         )}
         <button type="submit">Login</button>
       </Form>
+      {/* if error, show it */}
+        {isError ? <Error className= 'error'>{message}</Error>:
+          // if success message, show it
+          !isError && message ? <Message>{message}</Message> : null
+        }
+
     </div>
   );
 };
 
 const FormikLoginForm = withFormik({
 
-  mapPropsToValues({ password, username }) {
+  mapPropsToValues({ }) {
     return {
-      username: username || "",
-      password: password || ""
+
     };
   },
 
@@ -76,7 +102,12 @@ const FormikLoginForm = withFormik({
         console.log("success", response);
         setStatus(response.data);
         // resetForm();
-      });
+      })
+      .catch(err => {
+        setStatus('error');
+        console.log('Error: ', err);
+      })
+      setStatus('')
   }//end handleSubmit
 
 })(LoginForm);
